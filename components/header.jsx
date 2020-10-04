c66835144116e375e2040ec11a6bb19773eb8cfb
Faadhil Hakim <faadhil1991@Faadhils-MacBook-Pro.local>
2020-10-04 13:18:48 -0500

    add header component

---
+++ components/header.jsx
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <div className='flex bg-white py-3 px-2 shadow-sm border-b-2'>
+      <div className='w-5/12'>
+        <img
+          src='/logo.png'
+          alt='Nexttrack logo'
+          className='max-w-full h-auto'
+        />
+      </div>
+    </div>
+  )
+}
