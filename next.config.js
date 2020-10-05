37daf243d1cd4c380de73d914bdeef0d06e797ad
Faadhil Hakim <faadhil1991@Faadhils-MBP.attlocal.net>
2020-10-05 13:10:27 -0500

    redirect to blog

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 const withMDX = require('@next/mdx')()
 
-module.exports = withMDX()
+module.exports = {
+  withMDX,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/blog/jwt-authentication-node',
+        permanent: false,
+      },
+    ]
+  },
+}
